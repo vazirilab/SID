4fba7f79d36c31e1b141eb4aa41afc390298829d
Oliver Skocek <oskocek@rockefeller.edu>
2017-03-09 09:52:52 -0500

    repaired reconstruction_cpu_sparse

--- LFrecon_util/reconstruction_sparse.m
+++ LFrecon_util/reconstruction_sparse.m
@@ -62,61 +62,40 @@ zeroImageEx = gpuArray(zeros(exsize, 'single'));
 disp(['FFT size is ' num2str(exsize(1)) 'X' num2str(exsize(2))]);
 
 %% generate kernel
-<<<<<<< HEAD
 if ~isfield(options,'form')
     options.form='spherical';
 end
 
 if isfield(options,'rad')
-    if prod(options.rad)>0
-        if strcmp(options.form,'spherical')
-            W=zeros(2*options.rad(1)+1,2*options.rad(1)+1,2*options.rad(2)+1);
-            for ii=1:2*options.rad(1)+1
-                for jj=1:2*options.rad(1)+1
-                    for kk=1:2*options.rad(2)+1
-                        if  ((ii-(options.rad(1)+1))^2/options.rad(1)^2+(jj-(options.rad(1)+1))^2/options.rad(1)^2+(kk-(options.rad(2)+1))^2/options.rad(2)^2)<=1
-                            W(ii,jj,kk)=1;
-                        end
+    if strcmp(options.form,'spherical')
+        W=zeros(2*options.rad(1)+1,2*options.rad(1)+1,2*options.rad(2)+1);
+        for ii=1:2*options.rad(1)+1
+            for jj=1:2*options.rad(1)+1
+                for kk=1:2*options.rad(2)+1
+                    if  ((ii-(options.rad(1)+1))^2/options.rad(1)^2+(jj-(options.rad(1)+1))^2/options.rad(1)^2+(kk-(options.rad(2)+1))^2/options.rad(2)^2)<=1
+                        W(ii,jj,kk)=1;
                     end
                 end
-=======
-W=zeros(2*options.rad(1)+1,2*options.rad(1)+1,2*options.rad(2)+1);
-for ii=1:2*options.rad(1)+1
-    for jj=1:2*options.rad(1)+1
-        for kk=1:2*options.rad(2)+1
-            if  ((ii-(options.rad(1)+1))^2/options.rad(1)^2+(jj-(options.rad(1)+1))^2/options.rad(1)^2+(kk-(options.rad(2)+1))^2/options.rad(2)^2)<=1
-                W(ii,jj,kk)=1;
->>>>>>> refs/remotes/origin/master
             end
-        elseif strcmp(options.form,'gaussian')
-            gaussian=fspecial('gaussian',ceil(10*options.rad(1))+1,options.rad(1));
-            W=reshape(reshape(gaussian,[],1)*exp(-[-3*options.rad(2):1:3*options.rad(2)].^2/4/options.rad(2)^2),ceil(10*options.rad(1))+1,ceil(10*options.rad(1))+1,[]);
-            
-        elseif strcmp(options.form,'lorentz')
-            [X Y Z] = meshgrid([-ceil(5*options.rad(1)):ceil(5*options.rad(1))],[-ceil(5*options.rad(1)):ceil(5*options.rad(1))],[-ceil(5*options.rad(2)):ceil(5*options.rad(2))]);
-            W = 1./(1 + (options.rad(1)*(X.^2 + Y.^2) + options.rad(2)*Z.^2));
         end
-        W = W/norm(W(:));
-        kernel=gpuArray(W);
+    elseif strcmp(options.form,'gaussian')
+        gaussian=fspecial('gaussian',ceil(10*options.rad(1))+1,options.rad(1));
+        W=reshape(reshape(gaussian,[],1)*exp(-[-3*options.rad(2):1:3*options.rad(2)].^2/4/options.rad(2)^2),ceil(10*options.rad(1))+1,ceil(10*options.rad(1))+1,[]);
         
-        forwardFUN = @(Xguess) forwardFUN_(convn(Xguess,kernel,'same'));
-        backwardFUN = @(projection) convn(backwardFUN_(projection),kernel,'same');
-    else
-        forwardFUN = forwardFUN_;
-        backwardFUN = backwardFUN_;
+    elseif strcmp(options.form,'lorentz')
+        [X Y Z] = meshgrid([-ceil(5*options.rad(1)):ceil(5*options.rad(1))],[-ceil(5*options.rad(1)):ceil(5*options.rad(1))],[-ceil(5*options.rad(2)):ceil(5*options.rad(2))]);
+        W = 1./(1 + (options.rad(1)*(X.^2 + Y.^2) + options.rad(2)*Z.^2));    
     end
+    W = W/norm(W(:));
+    kernel=gpuArray(W);
+    
+    forwardFUN = @(Xguess) forwardFUN_(convn(Xguess,kernel,'same'));
+    backwardFUN = @(projection) convn(backwardFUN_(projection),kernel,'same');
     
 else
     forwardFUN = forwardFUN_;
     backwardFUN = backwardFUN_;
 end
-<<<<<<< HEAD
-=======
-kernel=gpuArray(W);
-
-forwardFUN = @(Xguess) forwardFUN_(convn(Xguess, kernel, 'same'));
-backwardFUN = @(projection) convn(backwardFUN_(projection), kernel, 'same');
->>>>>>> refs/remotes/origin/master
 
 %% Reconstruction
 LFIMG = single(in_file.LFmovie);
