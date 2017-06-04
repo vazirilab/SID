b73d742ef476c6653ea9c398b9c35ef33df35337
oliverskc <oliver.skocek@imp.ac.at>
2017-06-04 09:50:02 +0200

    bug fix
    change position of the def. of Varg to make sure it is defined in case
    frames does not exist

--- internal/incremental_temporal_update_gpu.m
+++ internal/incremental_temporal_update_gpu.m
@@ -11,9 +11,9 @@ end
 
 
 %% making sure what has already been computed does not get computed again
+Varg=ones(1,length(infiles_struct));
 if isfield(opts,'frame')
 opts.frame.end=min(opts.frame.end,length(infiles_struct));
-Varg=ones(1,length(infiles_struct));
 Varg(opts.frame.start:opts.frame.steps:opts.frame.end)=0;
 line=find(Varg);
 infiles_struct=infiles_struct(logical(Varg));
