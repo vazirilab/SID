23191785771c00c9933b167c5674fd67fdc07995
oliverskc <oskocek@rockefeller.edu>
2018-03-06 16:24:01 +0100

    bug fix in cellfilter
    fixed a typo and add the option to use the function without gpu support.

--- external/cellfilter.m
+++ external/cellfilter.m
@@ -1,4 +1,4 @@
-function filtered=cellfilter(stack,sigma,znonuniformity)
+function filtered=cellfilter(stack,sigma,znonuniformity,id)
 
 a=1.6;
 
@@ -6,25 +6,28 @@ filter1o=fspecial3_mod('gaussian',[floor(sigma/2)*2+1,floor(sigma/2)*2+1,max(3,f
 filter2o=fspecial3_mod('gaussian',[floor(sigma*a/2)*2+1,floor(sigma*a/2)*2+1,max(3,floor(sigma*a/znonuniformity/2)*2+1)],[sigma*a,sigma*a,sigma*a/znonuniformity]);
 
 if size(filter1o,1)>size(filter2o,1)
-    fitler2o=padarray(filter2o,[size(filter1o,1)-size(filter2o,1),0,0]/2);
+    filter2o=padarray(filter2o,[size(filter1o,1)-size(filter2o,1),0,0]/2);
 elseif size(filter1o,1)<size(filter2o,1)
     filter1o=padarray(filter1o,[size(filter2o,1)-size(filter1o,1),0,0]/2);
 end
 
 if size(filter1o,2)>size(filter2o,2)
-    fitler2o=padarray(filter2o,[0, size(filter1o,2)-size(filter2o,2),0]/2);
+    filter2o=padarray(filter2o,[0, size(filter1o,2)-size(filter2o,2),0]/2);
 elseif size(filter1o,2)<size(filter2o,2)
     filter1o=padarray(filter1o,[0, size(filter2o,2)-size(filter1o,2),0]/2);
 end
 
 if size(filter1o,3)>size(filter2o,3)
-    fitler2o=padarray(filter2o,[0, 0, size(filter1o,3)-size(filter2o,3)]/2);
+    filter2o=padarray(filter2o,[0, 0, size(filter1o,3)-size(filter2o,3)]/2);
 elseif size(filter1o,3)<size(filter2o,3)
     filter1o=padarray(filter1o,[0, 0, size(filter2o,3)-size(filter1o,3)]/2);
 end
 
-% size(filter1o)
-% size(filter2o)
+if id>0
+    filtered=gather(convn(gpuArray(stack),gpuArray(filter1o-filter2o),'same'));
+else
+    filtered=convn(stack,filter1o-filter2o,'same');
+end
 
-filtered=gather(convn(gpuArray(stack),gpuArray(filter1o-filter2o),'same'));
+end
 
