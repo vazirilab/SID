833fe420449d80cde38b8477e95717e006ee8f2c
oliverskc <oskocek@rockefeller.edu>
2018-03-11 06:29:31 +0100

    renamed a few variables

--- internal/compute_std_image.m
+++ internal/compute_std_image.m
@@ -1,13 +1,22 @@
-function std_image=compute_std_image(Y,bg_spatial,bg_temporal)
+function std_image=compute_std_image(Y,y_1,y_2)
+% COMPUTE_STD_IMAGE: Algorithm computes the standard deviation image of the
+% difference of a movie Y and a tensorproduct.
+%
+% Input: 
+% y_1...                    y_1 is a size(Y,1) times 1 array.
+% y_2...                    y_2 is a 1 times size(Y,2) array.
+%
+% Output:
+% std_image...              Standard deviation image
 
 if Y<2
-    bg_spatial=zeros(size(Y,1),1);
-    bg_temporal=zeros(1,size(Y,2));
+    y_1=zeros(size(Y,1),1);
+    y_2=zeros(1,size(Y,2));
 end
 
 A=var(Y,1,2);
-B=(bg_spatial.^2)*var(bg_temporal);
-C=(bg_spatial.^2-(sum(Y,2).*(bg_spatial*mean(bg_temporal))))/(length(bg_temporal)-1);
+B=(y_1.^2)*var(y_2);
+C=(y_1.^2-(sum(Y,2).*(y_1*mean(y_2))))/(length(y_2)-1);
 
 std_image=sqrt(A+B-2*C);
 end
