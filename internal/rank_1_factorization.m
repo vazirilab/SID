58ae39049c88745f6f943656c17b840d8c6ff1bb
oliverskc <oskocek@rockefeller.edu>
2018-03-11 06:30:35 +0100

    added help

--- internal/rank_1_factorization.m
+++ internal/rank_1_factorization.m
@@ -1,4 +1,15 @@
 function [bg_spatial,bg_temporal]=rank_1_factorization(Y,maxIter)
+% RANK_1_FACTORIZATION Rank-1-matrix-factorization of the movie Y
+%
+%       Y~bg_spatial*bg_temporal
+%
+% Input:
+% Y...                      movie
+% max_iter...               maximum Number of Iterations
+%
+% Output:
+% bg_temporal...            temporal component of the rank-1-factorization
+% bg_spatial...             spatial component of the rank-1-factorization
 
 if nargin<2
     maxIter=1;
