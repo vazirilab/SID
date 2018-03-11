3824a6d771f6df03b1afde3784b35f8a9ed8361e
oliverskc <oskocek@rockefeller.edu>
2018-03-11 08:15:00 +0100

    new function: find_kernel

---
+++ internal/find_kernel.m
@@ -0,0 +1,40 @@
+function kernel=find_kernel(Volume,Input)
+
+opts=struct;
+opts.border=[1,1,15];
+rr{1}=Volume;
+segmm=filter_recon(rr,Input,opts);
+segmm=max(segmm{1}-mean(segmm{1}(segmm{1}>0))/2,0);
+
+beads=bwconncomp(segmm);
+
+for k=1:beads.NumObjects
+    p(k)=norm(segmm(beads.PixelIdxList{k}));
+end
+
+[~,n]=sort(-p);
+n=n([1 min(length(n),11):length(n)]);
+for k=n
+    segmm(beads.PixelIdxList{k})=0;
+end
+
+centers=round(segment_component(segmm,0));
+
+id=(centers(:,1)>Input.neur_rad).*(centers(:,2)>Input.neur_rad).*...
+    (centers(:,3)>Input.neur_rad/Input.axial);
+centers=centers(id>0,:);
+id=(centers(:,1)<size(segmm,1)-Input.neur_rad).*(centers(:,2)<size(segmm,1)-Input.neur_rad).*...
+    (centers(:,3)<size(segmm,1)-Input.neur_rad/Input.axial);
+centers=centers(id>0,:);
+
+
+kernel=zeros(2*round(Input.neur_rad*[1 1 1/Input.axial])+1);
+for k=1:size(centers,1)
+    kernel=kernel+Volume(centers(k,1)-round(Input.neur_rad):centers(k,1)+round(Input.neur_rad),...
+        centers(k,2)-round(Input.neur_rad):centers(k,2)+round(Input.neur_rad),...
+        centers(k,3)-round(Input.neur_rad/Input.axial):centers(k,3)+round(Input.neur_rad/Input.axial));
+end
+end
+
+
+
