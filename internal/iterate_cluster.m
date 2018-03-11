11a26285c999c9a3d12eb770fffb8d7a851f0eae
oliverskc <oskocek@rockefeller.edu>
2018-03-11 06:30:11 +0100

    renamed variables

--- internal/iterate_cluster.m
+++ internal/iterate_cluster.m
@@ -1,19 +1,25 @@
-function [centroid, ID, centers]=iterate_cluster(centers_per_component,N,radius,dim)
-
+function [centroid, ID, centers]=iterate_cluster(centers_cell,N,radius,dim)
+% ITERATE_CLUSTER: Algorithm clusters the elements of the cell array
+% centers_cell, such that the clusters have a maximal size of
+% radius and each of their elements is from a different cell of
+% centers_cell.
+%
+% Input:
+% centers_cell...   cell array of array of size [m,3]
 centroid =[];
-for jj=1:size(centers_per_component,2)
-    centroid=[centroid' centers_per_component{jj}']';
+for jj=1:size(centers_cell,2)
+    centroid=[centroid' centers_cell{jj}']';
 end
 
 
 for nn=1:N
     centers=[];
     centroid_=[];
-    centers_per_component_=centers_per_component;
+    centers_per_component_=centers_cell;
     for ii=1:size(centroid,1)
         centers{ii}=[];
         for jj=1:size(centers_per_component_,2)
-            rnd_ind=randperm(size(centers_per_component{jj},1));
+            rnd_ind=randperm(size(centers_cell{jj},1));
             [a,n]=min(sum((dim.*(centers_per_component_{jj}(rnd_ind,:)-[centroid(ii,:)])).^2,2));
             if a<radius^2
                 centers{ii}=[centers{ii}' centers_per_component_{jj}(rnd_ind(n),:)']';
@@ -31,10 +37,10 @@ end
 [~,n] = sort(sum(centroid,2));
 
 if nargout>1
-    ID=false(size(centroid,1),size(centers_per_component,2));
+    ID=false(size(centroid,1),size(centers_cell,2));
     for ii=1:size(centroid,1)
         for jj=1:size(centers_per_component_,2)
-            a=min(sum((dim.*(centers_per_component{jj}-[centroid(ii,:)])).^2,2));
+            a=min(sum((dim.*(centers_cell{jj}-[centroid(ii,:)])).^2,2));
             if a<radius^2
                 ID(ii,jj)=true;
             end
