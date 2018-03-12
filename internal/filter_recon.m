effeaf5f5bd499e5e6a9355bfa4b2477a924d337
oliverskc <oskocek@rockefeller.edu>
2018-03-11 23:22:04 -0500

    bug fix & changed 2 standard params

--- internal/filter_recon.m
+++ internal/filter_recon.m
@@ -1,16 +1,16 @@
-function segmm=filter_recon(recon,Input,opts)
+function segmm=filter_recon(recon,opts)
 
-
+[~,u] = max([size(recon,1),size(recon,2)]);
 poolobj = gcp('nocreate');
 
-if nargin<3
+if nargin<2
     opts.border=[1 1 15];
-    opts.NumWorkers=length(Input.gpu_ids);
+    opts.NumWorkers=length(opts.gpu_ids);
 end
 
 if ~isfield(opts,'NumWorkers')
-    if isfield(Input,'gpu_ids')
-        opts.NumWorkers=length(Input.gpu_ids);
+    if isfield(opts,'gpu_ids')
+        opts.NumWorkers=length(opts.gpu_ids);
     else
         opts.NumWorkers=1;
     end
@@ -25,33 +25,33 @@ if isempty(poolobj)||(poolobj.NumWorkers~=opts.NumWorkers)
     parpool(opts.NumWorkers);
 end
 
-if isfield(Input,'gpu_ids')
-    n=length(Input.gpu_ids);
-    gimp=Input.gpu_ids;
+if isfield(opts,'gpu_ids')
+    n=length(opts.gpu_ids);
+    gimp=opts.gpu_ids;
 else
     n=1;
     gimp=-1;    
 end
 segmm=recon;
-gpu = ~isempty(Input.gpu_ids);
+gpu = ~isempty(opts.gpu_ids);
 
 
-for kk=1:opts.NumWorkers:size(recon,2)
+for kk=1:opts.NumWorkers:size(recon,u)
     img=cell(opts.NumWorkers,1);
     si_V=cell(opts.NumWorkers,1);
     siz_I=cell(opts.NumWorkers,1);
     segm_=cell(opts.NumWorkers,1);
-    for worker=1:min(opts.NumWorkers,size(recon,2)-(kk-1))
+    for worker=1:min(opts.NumWorkers,size(recon,u)-(kk-1))
         k=kk+worker-1;        
-        if Input.neur_rad<7
-            [X,Y,Z]=meshgrid(1:2:2*size(recon{k},2)-1,1:2:2*size(recon{k},1)-1,[1:Input.native_focal_plane-1 Input.native_focal_plane+1:size(recon{k},3)]);
+        if opts.neur_rad<5
+            [X,Y,Z]=meshgrid(1:2:2*size(recon{k},2)-1,1:2:2*size(recon{k},1)-1,[1:opts.native_focal_plane-1 opts.native_focal_plane+1:size(recon{k},3)]);
             [Xq,Yq,Zq]=meshgrid(1:1:2*size(recon{k},2)-1,1:1:2*size(recon{k},1)-1,1:size(recon{k},3));
-            cellSize = 4*Input.neur_rad;
+            cellSize = 4*opts.neur_rad;
         else
-            cellSize = 2*Input.neur_rad;
+            cellSize = 2*opts.neur_rad;
         end       
-        if Input.neur_rad<7
-            V=interp3(X,Y,Z,recon{k}(:,:,[1:Input.native_focal_plane-1 Input.native_focal_plane+1:size(recon{k},3)]),Xq,Yq,Zq);
+        if opts.neur_rad<5
+            V=interp3(X,Y,Z,recon{k}(:,:,[1:opts.native_focal_plane-1 opts.native_focal_plane+1:size(recon{k},3)]),Xq,Yq,Zq);
         else
             V=recon{k};
         end
@@ -65,30 +65,30 @@ for kk=1:opts.NumWorkers:size(recon,2)
         img{worker}=full(I/max(I(:)));
         siz_I{worker}=size(I);
     end
-
-    parfor worker=1:min(opts.NumWorkers,size(recon,2)-(kk-1))
-        filtered_Image_=band_pass_filter(img{worker}, cellSize, 8, gimp(mod(worker-1,n)+1),1.2);
+    parfor worker=1:min(opts.NumWorkers,size(recon,u)-(kk-1))
+        filtered_Image_=band_pass_filter(img{worker}, cellSize, 3, gimp(mod(worker-1,n)+1),1.2);
         segm_{worker}=filtered_Image_(opts.border(1):size(filtered_Image_,1)-opts.border(1),opts.border(2):size(filtered_Image_,2)-opts.border(2),opts.border(3)+1:opts.border(3)+si_V{worker});
         if gpu
             gpuDevice([]);
         end
     end
-    for kp=1:min(opts.NumWorkers,size(recon,2)-(kk-1))
+    for kp=1:min(opts.NumWorkers,size(recon,u)-(kk-1))
         filtered_Image=zeros(siz_I{kp}-[0 0 2*opts.border(3)]);
         filtered_Image(opts.border(1):siz_I{kp}(1)-opts.border(1),opts.border(2):siz_I{kp}(2)-opts.border(2),:)=segm_{kp};
-        if Input.neur_rad<7
+        if opts.neur_rad<5
             segmm{kk+kp-1}=filtered_Image(1:2:end,1:2:end,:);
         else
             segmm{kk+kp-1}=filtered_Image;
         end        
     end
-    disp(kk)
+    disp(['Filtering of Volumes from ' num2str(kk) ' to ' ...
+        num2str(min(kk+poolobj.NumWorkers-1,size(S,1))) ' completed']);
 end
 
-for ix=1:size(recon,2)
+for ix=1:size(recon,u)
     Vol = recon{ix}*0;
     Vol(opts.border(3):end-opts.border(3),opts.border(3):end-opts.border(3),:) = segmm{ix}(opts.border(3):end-opts.border(3),opts.border(3):end-opts.border(3),:);
-    segmm{ix} = Vol;
+    segmm{ix} = Vol/max(Vol(:));
 end
 
 end
