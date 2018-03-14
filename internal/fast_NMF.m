671fda78f075503e17ca100a0c2c47f096c099f3
oliverskc <oskocek@rockefeller.edu>
2018-03-14 06:29:16 -0500

    clean up code/ added lagrangian est

--- internal/fast_NMF.m
+++ internal/fast_NMF.m
@@ -1,7 +1,7 @@
 function [S,T]=fast_NMF(Y,n,opts,T,S)
-% FAST_NMF: the algorithm performs a non-negative matrix factorization 
+% FAST_NMF: the algorithm performs a non-negative matrix factorization
 % (nnmf) on the movie Y, using an alternating convex search
-% approach. Each of the updates is performed by gradient descent with 
+% approach. Each of the updates is performed by gradient descent with
 % exact line search.
 %
 % Input:
@@ -12,15 +12,15 @@ function [S,T]=fast_NMF(Y,n,opts,T,S)
 % S...                  Initial guess for the spatial compontent of the nnmf.
 %                       If S is not set, it is computed by S=Y*T
 % struct opts:
-% opts.lambda_s...      lagrangian multiplier for L1 regularizer on S
-% opts.lambda.t...      lagrangian multiplier for L1 regularizer on T
-% opts.lambda_ind_t...  lagrangian multiplier for L2 regularizer on
+% opts.lamb_spat...      lagrangian multiplier for L1 regularizer on S
+% opts.lambda.temp...      lagrangian multiplier for L1 regularizer on T
+% opts.lamb_corr...  lagrangian multiplier for L2 regularizer on
 %                       corrcoef(T)-eye(size(S,2))
-% opts.lambda_orth...   lagrangian multiplier for L2 regularizer on
+% opts.lamb_orth...   lagrangian multiplier for L2 regularizer on
 %                       S'*S-eye(size(S,2))
-% opts.lambda_sm...     lagrangian multiplier for L2 regularizer on the
+% opts.lamb_TotVarSpat...     lagrangian multiplier for L2 regularizer on the
 %                       total variation of the components of S.
-%                       It is necessary that the size (height & width) of 
+%                       It is necessary that the size (height & width) of
 %                       the linearized components of S is included, since
 %                       the algorithm needs the reshape S accordingly.
 % opts.ini...           Initialization method for T. opts.ini='pca' uses
@@ -28,101 +28,115 @@ function [S,T]=fast_NMF(Y,n,opts,T,S)
 %                       generates "n" smoothed random traces as
 %                       initialization for T.
 % opts.max_iter...      maximum number of iterations
+% opts.lb...            Integer; Lower bound for the number of non-negative
+%                       pixels per spatial component.
 %%
-if nargin<4
-    T=rand(n,size(Y,2));
-    T=conv2(T,ones(1,32),'same');    
-end
 if nargin<3
-    opts.lambda_s=0;
-    opts.lambda_t=0;
-    opts.lambda_ind_t=0;
-    opts.lambda_orth=0;
-    opts.lambda_sm=0;
+    opts.lamb_spat=0;
+    opts.lamb_temp=0;
+    opts.lamb_corr=0;
+    opts.lamb_orth=0;
+    opts.lamb_TotVarSpat=0;
     opts.ini='pca';
 else
-    if ~isfield(opts,'lambda_s')
-        opts.lambda_s=0;
+    if ~isfield(opts,'lamb_spat')
+        opts.lamb_spat=0;
     end
-    if ~isfield(opts,'lambda_t')
-        opts.lambda_t=0;
+    if ~isfield(opts,'lamb_temp')
+        opts.lamb_temp=0;
     end
-    if ~isfield(opts,'lambda_ind_t')
-        opts.lambda_ind_t=0;
+    if ~isfield(opts,'lamb_corr')
+        opts.lamb_corr=0;
     end
-    if ~isfield(opts,'lambda_orth')
-        opts.lambda_orth=0;
+    if ~isfield(opts,'lamb_orth')
+        opts.lamb_orth=0;
     end
     if ~isfield(opts,'active')
         opts.active = ones(1,size(Y,1),'logical');
     end
-    if ~isfield(opts,'lambda_sm')
-        opts.lambda_sm=0;
+    if ~isfield(opts,'lamb_TotVarSpat')
+        opts.lamb_TotVarSpat=0;
     end
     if ~isfield(opts,'ini')
         opts.ini='pca';
     end
+    if ~isfield(opts,'lb')
+        opts.lb = 133;
+    end
 end
 
-if opts.lambda_sm
+if opts.lamb_TotVarSpat
     if ~isfield(opts,'size')
-        disp('The option lambda_sm needs additional information: imagesize (opts.size)');
+        disp('The option lamb_TotVarSpat needs additional information: size of the image (opts.size)');
         return
     end
-   lap = zeros(1,3,3);
-   lap(1,2,2)=4;
-   lap(1,1,2)=-1;
-   lap(1,3,2)=-1;
-   lap(1,2,1)=-1;
-   lap(1,2,3)=-1;
+    laplace = zeros(1,3,3);
+    laplace(1,2,2)=4;
+    laplace(1,1,2)=-1;
+    laplace(1,3,2)=-1;
+    laplace(1,2,1)=-1;
+    laplace(1,2,3)=-1;
 end
-   
 
-opts.lambda_orth = opts.lambda_orth * norm(Y(:));
 
-opts.warm_start=[];
+opts.lamb_orth = opts.lamb_orth * norm(Y(:))*size(Y,2);
+opts.lamb_spat = opts.lamb_spat * norm(Y(:))*size(Y,2);
+opts.lamb_temp = opts.lamb_temp * norm(Y(:));
+opts.lamb_corr = opts.lamb_corr * norm(Y(:));
+opts.lamb_TotVarSpat = opts.lamb_TotVarSpat * norm(Y(:));
 
-option=opts;
-option.max_iter=1;
-option.total=1;
+flag_t = false;
+flag_s = false;
 
 if ~n
     opts.max_iter=0;
 end
 
-
-% opts.warm_start=[];
-% option=opts;
-% option.max_iter=1;
-% option.lambda=opts.lambda_s;
-
 if nargin<4
     if strcmp(opts.ini,'rand')
-        T=rand(n,size(Y,2));
-        T=conv2(T,ones(1,32),'same');
+        T_0=rand(n,size(Y,2));
+        T_0=conv2(T_0,ones(1,32),'same');
     else
-        [T,~,~] = pca(Y(opts.active,:));
-        T = abs(T(:,1:n))';
+        [T_0,~,~] = pca(Y(opts.active,:));
+        T_0 = abs(T_0(:,1:n))';
     end
+    T = T_0;
 end
 
 if nargin<5
-%     S=fast_nnls(T',Y',option)';
     S=Y*T';
-%     S=S*inv(diag(sqrt(sum(S.^2,1))));
 end
-N=0;
 
-
-for iter=1:opts.max_iter + N
-    
-    %     tic
+iter = 0;
+while iter<opts.max_iter
+    iter = iter + 1;
     T(isnan(T))=0;
     line = ~logical(sum(T,2));
     
     if max(line)
-        T(line,:) = rand(sum(line),size(T,2));
-        N=N+400;
+        T = T_0;
+        S=Y*T';
+        opts.lamb_spat = opts.lamb_spat/10;
+        opts.lamb_temp = opts.lamb_temp/10;
+        opts.lamb_corr = opts.lamb_corr/10;
+        opts.lamb_orth = opts.lamb_orth/10;
+        opts.lamb_TotVarSpat = opts.lamb_TotVarSpat/10;
+        disp(opts);
+        iter = 0;
+        flag_t = true;
+        disp('ZERO detected - ReInitializing');
+        continue
+    elseif flag_t
+        T = T_0;
+        S=Y*T';
+        opts.lamb_spat = opts.lamb_spat/10;
+        opts.lamb_temp = opts.lamb_temp/10;
+        opts.lamb_corr = opts.lamb_corr/10;
+        opts.lamb_orth = opts.lamb_orth/10;
+        opts.lamb_TotVarSpat = opts.lamb_TotVarSpat/10;
+        disp(opts);
+        iter = 0;
+        flag_t = false;
     end
     
     for u=1:size(T,1)
@@ -138,12 +152,12 @@ for iter=1:opts.max_iter + N
     Q_S = S(opts.active,:)'*S(opts.active,:);
     q_T = S(opts.active,:)'*Y(opts.active,:);
     
-    if opts.lambda_ind_t
+    if opts.lamb_corr
         hilf = ((zsc*zsc')*zsc - zsc);
         hilf = diag(1./d)*(hilf - (diag(sum(hilf,2))*ones(size(T))/N + (diag(diag(hilf*zsc'))*zsc)/N));
-        df_T = -q_T + Q_S*T + opts.lambda_t + opts.lambda_ind_t*hilf;
+        df_T = -q_T + Q_S*T + opts.lamb_temp + opts.lamb_corr*hilf;
     else
-        df_T = -q_T + Q_S*T + opts.lambda_t;
+        df_T = -q_T + Q_S*T + opts.lamb_temp;
     end
     
     passive_T = max(T>0,df_T<0);
@@ -159,7 +173,7 @@ for iter=1:opts.max_iter + N
     end
     
     T(T<0)=0;
-    if opts.lambda_orth
+    if opts.lamb_orth
         for u=1:size(T,1)
             platz = norm(S(:,u));
             T(u,:) = T(u,:)*platz;
@@ -167,35 +181,51 @@ for iter=1:opts.max_iter + N
         end
     end
     
-    
-    %             norm(reshape(Y-S*T,1,[]))
-    
-    %     disp('!')
     S(isnan(S))=0;
-    line = ~logical(sum(S,1));
     
     
+    line = ~logical(sum(S,1).*(sum(S>0,1)>opts.lb));
+
     if max(line)
-        S(:,line) = rand(size(S,1),sum(line));
-        N=N+400;
+        T = T_0;
+        S=Y*T';
+        opts.lamb_spat = opts.lamb_spat/10;
+        opts.lamb_temp = opts.lamb_temp/10;
+        opts.lamb_corr = opts.lamb_corr/10;
+        opts.lamb_orth = opts.lamb_orth/10;
+        opts.lamb_TotVarSpat = opts.lamb_TotVarSpat/10;
+        disp(opts);
+        iter = 0;
+        flag_s = true;
+        disp('ZERO detected - ReInitializing');
+        continue
+    elseif flag_s
+        T = T_0;
+        S=Y*T';
+        opts.lamb_spat = opts.lamb_spat/10;
+        opts.lamb_temp = opts.lamb_temp/10;
+        opts.lamb_corr = opts.lamb_corr/10;
+        opts.lamb_orth = opts.lamb_orth/10;
+        opts.lamb_TotVarSpat = opts.lamb_TotVarSpat/10;
+        disp(opts);
+        iter = 0;
+        flag_s = false;
     end
     
     Q_T = T*T';
     q_S = Y*T';
     
-    if opts.lambda_orth
-        
-        df_S = -q_S + S*Q_T + opts.lambda_s + opts.lambda_orth*(S*(S'*S)-S);
+    if opts.lamb_orth
+        df_S = -q_S + S*Q_T + opts.lamb_spat + opts.lamb_orth*(S*(S'*S)-S);
     else
-        df_S = -q_S + S*Q_T + opts.lambda_s;
+        df_S = -q_S + S*Q_T + opts.lamb_spat;
     end
     
-    if opts.lambda_sm
-       df_S = df_S + opts.lambda_sm*reshape(convn(reshape(S',n,opts.size(1),opts.size(2)),lap,'same'),n,[])'; 
+    if opts.lamb_TotVarSpat
+        df_S = df_S + opts.lamb_TotVarSpat*reshape(convn(reshape(S',n,opts.size(1),opts.size(2)),laplace,'same'),n,[])';
     end
     
     passive_S = max(S>0,df_S<0);
-    
     
     df_S_ = passive_S.*df_S;
     
@@ -207,14 +237,11 @@ for iter=1:opts.max_iter + N
     end
     
     if (max(df_S_(:))==0)&&(max(df_T_(:))==0)
+        disp('Terminated!')
         return;
     end
     
     S(S<0)=0;
-    %     sum(sum(T.*(Q_S*T),1),2) + sum(sum(T.*(-q_T)))
-    
-    %     toc
-    %         norm(reshape(Y-S*T,1,[]))
     
     disp(['Iteration ' num2str(iter) ' completed']);
 end
