38a2b4840072622151d0464b3a444c8810f975c7
Tobias Noebauer <tobias.noebauer@imp.ac.at>
2018-05-29 08:19:37 -0400

    Fix latex equations in rank_1_factorization.m

--- src/rank_1_factorization.m
+++ src/rank_1_factorization.m
@@ -1,7 +1,6 @@
-function [bg_spatial,bg_temporal]=rank_1_factorization(Y,maxIter)
-%% RANK_1_FACTORIZATION Rank-1-matrix-factorization of the movie Y
-%
-%       Y~bg_spatial*bg_temporal
+function [bg_spatial, bg_temporal] = rank_1_factorization(Y, maxIter)
+%% RANK_1_FACTORIZATION  Rank-1-matrix-factorization of the movie Y
+%       Y ~ bg_spatial * bg_temporal
 %
 % Input:
 % Y                      movie
@@ -12,11 +11,16 @@ function [bg_spatial,bg_temporal]=rank_1_factorization(Y,maxIter)
 % bg_spatial             spatial component of the rank-1-factorization
 %
 % This algorithm performs a form a block-wise gradient descent on the objective function
-% $$L\\left( s,t \\right) = \\sum\_{\\text{ij}}^{}\\left( Y\_{\\text{ij}} - s\_{i}t\_{j} \\right)^{2}$$
-% Here, s corresponds to *bg\_spatial*, and t corresponds to *bg\_temporal*.
-% This can be seen when we calculate the gradient along s and t:
-% *D*<sub>*s*</sub>*L* = 2(||*s*||<sub>2</sub><sup>2</sup>*s* − *s* \* *Y*)
-% *D*<sub>*t*</sub>*L* = 2(||*t*||<sub>2</sub><sup>2</sup>*t* − *Y* \* *t*)
+%
+% $$L \left( s, t \right) = \sum_{i j}^{} \left( Y_{i j} - s_i t_j \right) ^2$$
+% 
+% Here, s corresponds to *bg_spatial*, and t corresponds to *bg_temporal*.
+% This can be seen by calculating the gradient along s and t
+% 
+% $$D_s L=2\left( \|s\|_2^2\,s - s\,Y\right)$$
+% 
+% $$D_t L=2\left( \|t\|_2^2\,t - t\,Y\right)$$
+%
 % and set them to zero. Between update we normalize the previously updated component. 
 % This simplifies the code and leads to better performance in the general case of NNMF.
 
