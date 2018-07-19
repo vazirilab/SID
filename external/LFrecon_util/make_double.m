8b74b529af11cba4bb68f5083d1ce5687c730b3b
Tobias Noebauer <tobias.noebauer@imp.ac.at>
2018-07-19 17:07:02 -0400

    Remove unused files

--- external/LFrecon_util/make_double.m
+++
@@ -1,9 +0,0 @@
-function [ ret ] = make_double( x )
-if isa(x, 'double')
-    ret = x;
-elseif strfind(x, ',') % try to parse as comma-separated list of doubles
-    ret = str2double(strsplit(x, ','));
-else
-    ret = str2double(x);
-end
-
